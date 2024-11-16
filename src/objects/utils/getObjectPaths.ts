a0ecbeeeac1c3c6f93ad6dc8d277289ee7593c0f
Wojciech Morawski <wojciech.morawski@ipfdigital.com>
2024-11-16 17:10:03 +0100

    fix: record to object type

--- src/objects/utils/getObjectPaths.ts
+++ src/objects/utils/getObjectPaths.ts
@@ -1,23 +1,31 @@
-import type { ConsistentDeepObj } from "../../testData";
+// prettier-ignore
+import type { ArrObjArrObj, ConsistentDeepObj } from "../../testData";
 
 // TODO: what is the name for this pattern?
 export type DecrementDepth = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // TODO: add support for arrays
 export type GetObjPaths<
-  T extends Record<string, any>,
+  T extends object,
   Depth extends number = 10
 > = Depth extends never
   ? never
+  : T extends any[]
+  ? never
   : T extends object
   ? {
-      [K in keyof T]:
-        | K
-        | (T[K] extends object
-            ? `${K & string}.${GetObjPaths<T[K], DecrementDepth[Depth]>}`
-            : never);
+      [K in keyof T]: K | K extends string
+        ? T[K] extends object
+          ? `${K}.${GetObjPaths<T[K], DecrementDepth[Depth]>}`
+          : K
+        : never;
     }[keyof T]
   : never;
 
+// TESTS
 // TODO: add non nullable to remove undefined from union
-type Test = GetObjPaths<ConsistentDeepObj>;
+type TestA = GetObjPaths<ConsistentDeepObj>;
+//   ^?
+
+type TestB = GetObjPaths<ArrObjArrObj>;
+//   ^?
