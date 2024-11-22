f682889678c580d65cb33ccbd549e01192fea11d
Wojciech Morawski <wojciech.morawski@ipfdigital.com>
2024-11-22 08:20:52 +0100

    chore: ReverseString cleanup

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,15 +1,6 @@
-
-
 // string to union --------------------------------
 type StringToUnion<S, Union = never> = S extends `${infer First}${infer Rest}` ? StringToUnion<Rest, Union | First> : Union
 type test = StringToUnion<'abcde'>
-//   ^?
-
-// reverse string --------------------------------
-type ReverseString<T extends string, Acc extends string = ''> = T extends `${infer First}${infer Last}`
-? ReverseString<Last, `${First}${Acc}`>
-: Acc
-type testA = ReverseString<'asdf'>
 //   ^?
 
 export type InferPre<
