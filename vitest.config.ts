0d0536d68783356f42d3c02806472b5cd5d051fa
HideoKun <wo.contact@gmail.com>
2024-11-28 10:52:39 +0100

    fix: tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     css: false, // This disables CSS processing during tests,
+    include: ['src/**/*.test.ts'],
     coverage: {
       reporter: ['text', 'json', 'html', 'lcov'],
     },
