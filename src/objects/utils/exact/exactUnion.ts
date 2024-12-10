6d155e9fbe392455b4793e81b88eb55dd23bbb57
HideoKun <wo.contact@gmail.com>
2024-12-10 17:52:34 +0100

    chore: rename

--- src/objects/utils/exact/exactUnion.ts
+++ src/objects/utils/exact/exactUnion.ts
@@ -1,22 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // TODO: IsExactUnion -> boolean
-export type ExactUnion<A, B> = [A] extends [B]
+export type IsExactUnion<A, B> = [A] extends [B]
   ? [B] extends [A]
     ? true
     : false
   : false;
 
 // TESTS
-type TestUnionA = ExactUnion<"a" | "b", "a" | "b">;
+type TestUnionA = IsExactUnion<"a" | 1, "a" | 1>;
 //   ^?
 const testUnionA: TestUnionA = true;
 
-type TestUnionB = ExactUnion<"a" | "b" | "c", "a" | "b">;
+type TestUnionB = IsExactUnion<"a" | "b" | "c", "a" | "b">;
 //   ^?
 // @ts-expect-error
 const testUnionB: TestUnionB = "a";
 
-type TestUnionC = ExactUnion<"a" | "b", "a" | "b" | "c">;
+type TestUnionC = IsExactUnion<"a" | "b", "a" | "b" | "c">;
 //   ^?
 // @ts-expect-error
 const testUnionC: TestUnionC = "a";
