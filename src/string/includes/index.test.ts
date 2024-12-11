998548d2ea4298635fe524c0632aa5acbfb87d07
HideoKun <wo.contact@gmail.com>
2024-12-11 08:13:45 +0100

    fix: includes

--- src/string/includes/index.test.ts
+++ src/string/includes/index.test.ts
@@ -2,20 +2,16 @@ import { expectTypeOf, it } from "vitest";
 import type { Includes } from ".";
 
 it("should work", () => {
-  expectTypeOf<Includes<"belladonna", "lladon">>().toEqualTypeOf<true>();
-  expectTypeOf<Includes<"teczuszka", "uszka">>().toEqualTypeOf<true>();
-  expectTypeOf<Includes<"maciuś", "ś">>().toEqualTypeOf<true>();
-
-  expectTypeOf<Includes<"belladonna", "xxx">>().toEqualTypeOf<false>();
-  expectTypeOf<Includes<"teczuszka", "nic">>().toEqualTypeOf<false>();
-  expectTypeOf<Includes<"maciuś", "s">>().toEqualTypeOf<false>();
+  expectTypeOf<Includes<"lladon", "belladonna">>().toEqualTypeOf<true>();
+  expectTypeOf<Includes<"uszka", "teczuszka">>().toEqualTypeOf<true>();
+  expectTypeOf<Includes<"ś", "maciuś">>().toEqualTypeOf<true>();
 });
 
 it("should not work", () => {
   // @ts-expect-error
-  expectTypeOf<Includes<"belladonna", "xxx">>().toEqualTypeOf<true>();
+  expectTypeOf<Includes<"xxx", "belladonna">>().toEqualTypeOf<true>();
   // @ts-expect-error
-  expectTypeOf<Includes<"teczuszka", "nic">>().toEqualTypeOf<true>();
+  expectTypeOf<Includes<"nic", "teczuszka">>().toEqualTypeOf<true>();
   // @ts-expect-error
-  expectTypeOf<Includes<"maciuś", "s">>().toEqualTypeOf<true>();
+  expectTypeOf<Includes<"s", "maciuś">>().toEqualTypeOf<true>();
 });
--- src/string/includes/index.ts
+++ src/string/includes/index.ts
@@ -4,24 +4,32 @@ import type { Str2Arr } from "../str2arr";
  * A type that checkes if a substring is a part of a string
  *
  * @template Str - The string type to check against.
- * @template SearchStr - The substring to check for or substring with wildcard.
+ * @template MatchStr - The substring to check for or substring with wildcard.
  * @returns {true | false} - Returns true if search string is a part of the string or false if its not
  */
 
-export type Includes<
-  Str extends string,
+export type Includes<MatchStr extends string, Str extends string> =
+  //
+  _Includes<MatchStr, Str, "">;
+
+type UpdateAcc<
   SearchStr extends string,
-  MatchAccumulator extends string = "",
-  ReferenceArray extends string[] = [],
+  MatchFirst extends string,
+  MatchAccumulator extends string,
+> = MatchFirst extends Str2Arr<SearchStr>[Str2Arr<MatchAccumulator>["length"]]
+  ? `${MatchAccumulator}${MatchFirst}` // build match step by step
+  : "";
+
+export type _Includes<
+  SearchStr extends string,
+  MatchStr extends string,
+  MatchAccumulator extends string,
 > = SearchStr extends MatchAccumulator
   ? true
-  : Str extends `${infer First}${infer Rest}`
-    ? First extends Str2Arr<SearchStr>[ReferenceArray["length"]]
-      ? Includes<
-          Rest,
-          SearchStr,
-          `${MatchAccumulator}${First}`,
-          [...ReferenceArray, First]
-        >
-      : Includes<Rest, SearchStr, "", []>
+  : MatchStr extends `${infer MatchFirst}${infer MatchRest}`
+    ? _Includes<
+        SearchStr,
+        MatchRest,
+        UpdateAcc<SearchStr, MatchFirst, MatchAccumulator>
+      >
     : false;
